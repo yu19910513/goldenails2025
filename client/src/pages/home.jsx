91026d8c37c30b03f01a1df45766e9fbb23baa50
Rex Yu <yu19910513@gmail.com>
2025-01-20 10:30:14 -0800

    add serviceCards

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,12 +3,14 @@ import Hero from '../components/home_page/Hero';
 import SpaLocation from '../components/home_page/SpaLocation';
 import NailServiceIntro from '../components/home_page/NailServiceIntro';
 import PartnerSection from '../components/home_page/PartnerSection';
+import ServiceCards from '../components/home_page/ServiceCards';
 
 // Main App Component
 const Home = () => (
   <div>
     <Hero />
     <NailServiceIntro />
+    <ServiceCards />
     <PartnerSection />
     <SpaLocation />
   </div>
