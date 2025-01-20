15015988f3119e7a1c062dc9d9c554f54cf6e0c1
Rex Yu <yu19910513@gmail.com>
2025-01-19 21:37:52 -0800

    add squidio

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,12 +2,14 @@ import { React } from 'react'
 import Hero from '../components/home_page/Hero';
 import SpaLocation from '../components/home_page/SpaLocation';
 import NailServiceIntro from '../components/home_page/NailServiceIntro';
+import PartnerSection from '../components/home_page/PartnerSection';
 
 // Main App Component
 const Home = () => (
   <div>
     <Hero />
     <NailServiceIntro />
+    <PartnerSection />
     <SpaLocation />
   </div>
 );
