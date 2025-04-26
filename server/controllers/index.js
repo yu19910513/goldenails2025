525ac67bef6b1b5f8ed61be5f3a6ea53df7cf7b9
Rex Yu <yu19910513@gmail.com>
2025-04-26 15:49:39 -0700

    add js doc and fix jest test

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,11 @@
+/**
+ * Main router for the application.
+ * 
+ * - Applies basic authentication middleware to all `/api` routes.
+ * - Delegates `/api` requests to the `api` router.
+ * 
+ * @module router
+ */
 const router = require('express').Router();
 const api = require('./api');
 const { basic_auth } = require('../utils/authentication');
