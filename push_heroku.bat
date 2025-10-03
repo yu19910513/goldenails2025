661fd7e71a271e371dfd0a15dc515d672e80e0aa
rex yu <yu19910513@gmail.com>
2025-10-03 14:39:53 -0700

    fixed npm package

--- push_heroku.bat
+++ push_heroku.bat
@@ -1,26 +1,17 @@
 @echo off
 REM ============================
-REM Heroku login and push to Heroku
+REM Push to Heroku using CLI token (skip login)
 REM ============================
 
-REM Step 1: Login to Heroku
-echo Logging into Heroku...
-heroku login
-if errorlevel 1 (
-    echo Heroku login failed. Exiting.
-    pause
-    exit /b 1
-)
-
-REM Step 2: Get Heroku Git token
+REM Step 1: Get Heroku Git token
 for /f "delims=" %%t in ('heroku auth:token') do set HEROKU_TOKEN=%%t
 if "%HEROKU_TOKEN%"=="" (
-    echo Failed to get Heroku token. Exiting.
+    echo Failed to get Heroku token. Make sure you are logged into Heroku CLI.
     pause
     exit /b 1
 )
 
-REM Step 3: Push current branch to Heroku main
+REM Step 2: Push current branch to Heroku main
 echo Pushing to Heroku using token...
 git push https://heroku:%HEROKU_TOKEN%@git.heroku.com/goldenails2025.git main
 
